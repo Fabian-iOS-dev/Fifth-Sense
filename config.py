465b9b1b7b0307abe7e5fe7279d817c6eecc164b
Julian Kopp <Julian.Kopp@de.ibm.com>
2018-08-29 08:47:48 +0200

    current config

--- config.py
+++ config.py
@@ -1,14 +1,19 @@
 class Config:
     # vgg16 takes 224 as input
-    width = 200
-    height = 200
+    width = 224
+    height = 224 #round(1.5*width)
 
-    classes = 6
+    classes = 12
 
     dataset = 'ds_normal'
+
+    epochs = 10
     batch_size = 16
 
+    #needs to be an int
+    steps = 2000/batch_size
+
     #add model in elif struct in start_train
-    #model, model_I
-    model = 'model_I'
+    #model 1, model 2, model 3
+    model = 'model 2'
 
