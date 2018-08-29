2d3e7f54c3e0074c32b3d49a8d40314114c957fd
Julian Kopp <Julian.Kopp@de.ibm.com>
2018-08-29 08:45:18 +0200

    12 classes instead of 6

--- create_datasets.py
+++ create_datasets.py
@@ -1,17 +1,18 @@
 import os
 import random
-from resize_images import list_dirs
+from old_scripts.resize_images import list_dirs
 from PIL import Image
 from math import floor
+# from crop_banknote import crop
 
 #!!!!! naming convention: start with 'ds' and then characterise the dataset in the name
 #'ds*/' directories are ignored in Github for overview reasons
 ds_name = os.path.join(os.getcwd(), 'ds_normal') #name of the dataset generated
 
-categories = ['normal'] #from which categories pictures should be sampled
+categories = ['normal', 'angle'] #from which categories pictures should be sampled
 perc_train = 0.8
 
-banknotes = list_dirs(os.path.join(os.getcwd(),'dataset'))
+banknotes = list_dirs(os.path.join(os.getcwd(),'dataset_v3')) #data comes from this directory
 
 #create tree structure for dataset
 try:
@@ -20,7 +21,8 @@ except FileExistsError:
         print('Directory ',ds_name + ' already exits. Please rename the variable "ds_name" or delete the directory in question.')
         raise
 
-for note in ['500', '1000', '2000', '5000', '10000', '20000']:
+for note in ['500b', '1000b', '2000b', '5000b', '10000b', '20000b',
+             '500f', '1000f', '2000f', '5000f', '10000f', '20000f']:
         os.makedirs(os.path.join(ds_name,'train',note))
         os.makedirs(os.path.join(ds_name, 'test', note))
 
@@ -43,9 +45,14 @@ for banknote in banknotes:
             save_location = os.path.join(ds_name, 'train', bank_note_str[1], file_name[1])
             img.save(save_location)
 
+            # crop(pic, save_location, bank_note_str[1])
+
+
         for pic in test_pics:
             img = Image.open(pic)
             file_name = os.path.split(pic)
             bank_note_str = os.path.split(banknote)
             save_location = os.path.join(ds_name, 'test', bank_note_str[1], file_name[1])
             img.save(save_location)
+
+            # crop(pic, save_location, bank_note_str[1])
