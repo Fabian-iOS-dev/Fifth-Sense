c57256372a7c04fc5dc85a44dd07f210dd21d78e
Julian Kopp <Julian.Kopp@de.ibm.com>
2018-09-04 11:42:46 +0200

    updated vgg

--- model_2.py
+++ model_2.py
@@ -1,26 +1,38 @@
-from keras.models import Model
-from keras.layers import Dense
+from keras.models import Model, Sequential
+from keras.layers import Dense, Flatten
 from config import Config as C
 from keras.applications.vgg16 import VGG16
+from numpy import random
 
 #VGG16
 
+seed = random.seed(300)
 
+vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=(3, C.height, C.width))
 
 # Generate a model with all layers (with top)
-vgg16 = VGG16(weights='imagenet', include_top=True, input_shape=(3, C.height, C.width))
+output_model = Sequential()
+output_model.add(Flatten())
+output_model.add(Dense(4096, activation='relu'))
+output_model.add(Dense(C.classes, activation='softmax', name='predictions'))
 
-#Add a layer where input is the output of the  second last layer
-x = Dense(C.classes, activation='softmax', name='predictions')(vgg16.layers[-2].output)
 
 #Then create the corresponding model
-model = Model(input=vgg16.input, output=x)
+# this works but layers of output model are not shown
+# model = Model(input=vgg16.input, output=output_model(vgg16.output))
+
+model = Sequential() #model is the vgg16 architecture with modified output
+for l in vgg16.layers:
+    model.add(l)
+
+for l in output_model.layers:
+    model.add(l)
+
+for layer in model.layers[:16]:
+    layer.trainable = False
+
 model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.save('my_model.h5') #save model (architecture + weights + optimizer state)
-
-
-
-
